0cd438be5fd2d43b0e7404f608b62ca13bea991f
Jeremy Banks <jeremy@jeremybanks.ca>
2011-07-15 20:04:33 -0400

    SQLite example

--- traqk.py
+++ traqk.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python2.7
 from bottle import route, run
+import sqlite3
+
+db = sqlite3.connect("database.db")
+db.execute("""CREATE TABLE IF NOT EXISTS
+              "example" (key TEXT PRIMARY KEY, value TEXT)""")
+
+with db:
+    db.execute("""INSERT INTO example VALUES ("hello", "world")""")
+    db.execute("""INSERT INTO example VALUES ("world", "hello")""")
 
 @route("/hello")
 def hello():
