627be7aef542f46cee91a39c2fba03d520a173f7
Jeremy Banks <jeremy@jeremybanks.ca>
2011-07-30 23:40:51 -0400

    m

--- lib/sqlite-bridge/server.py
+++ lib/sqlite-bridge/server.py
@@ -7,11 +7,13 @@ import threading
 import queue
 
 class SQLiteServer(object):
-    QUEUE_SIZE = 0
+    QUEUE_SIZE = 12
     
-    def __init__(self, in_stream, out_stream):
+    def __init__(self, in_stream, out_stream, filename=":memory:"):
         self.in_stream = in_stream
         self.out_stream = out_stream
+        self.filename = filename
+        
         self.working = False
         self.in_queue = queue.Queue(maxsize=self.QUEUE_SIZE)
         self.out_queue = queue.Queue(maxsize=self.QUEUE_SIZE)
@@ -32,28 +34,41 @@ class SQLiteServer(object):
         self.output_thread.start()
     
     def do_work(self):
+        self.connection = sqlite3.connect(self.filename)
+        
         while self.working:
-            sys.stderr.write("DOING WORK\n")
             self.out_queue.put(self._do_work(self.in_queue.get()))
-            sys.stderr.write("DONE WORK\n")
+            self.in_queue.task_done()
     
     def _do_work(self, work):
-        return {"input": work, "id": work["id"]}
+        query = work["params"]["query"]
+        
+        try:
+            with self.connection as c:
+                result = list(c.execute(query).fetchall())
+                        
+            return {"result": result, "error": None, "id": work["id"]}
+        except Exception as e:
+            sys.stderr.write(str(e) + "\n")
+            sys.stderr.flush()
+            return {"result": None, "error": str(e), "id": work["id"]}      
     
     def do_input(self):
-        while True:
-            # self.in_queue.put(json.load(self.in_stream))
-            line = self.in_stream.readline()
-            sys.stderr.write("rea line....\n")
+        for line in self.in_stream:
             o = json.loads(line)
             self.in_queue.put(o)
-            sys.stderr.write("PUT!!")
+        
+        self.working = False
     
     def do_output(self):
         while True:
-            json.dump(self.out_queue.get(), self.out_stream)
-            sys.stderr.write("wrote line\n")
+            value = self.out_queue.get()
+            json.dump(value, self.out_stream)
+            sys.stderr.write(str(value))
+            sys.stderr.flush()
             self.out_stream.write("\n")
+            self.out_stream.flush()
+            self.out_queue.task_done()
 
 def quote_identifier(s, errors="strict"):
     """Quotes a SQLite identifier."""
